15962f6178d87d2720df3ab5600548095321337c
Sabbir Ahmed <sabbir_id@outlook.com>
2020-03-13 17:21:04 +0900

    Router 修正

--- core/Router.php
+++ core/Router.php
@@ -1,68 +1,84 @@
 <?php
 
-class Router {
+/**
+ * Router
+ *
+ * PHP version 5.4
+ */
+class Router
+{
 
-    // Associative array of routes
-    // @var array
+    /**
+     * Associative array of routes (the routing table)
+     * @var array
+     */
     protected $routes = [];
 
     /**
-    * Parameters from the matched route
+     * Parameters from the matched route
+     * @var array
     * @var array 
-    **/
+     * @var array
+     */
     protected $params = [];
 
-    /* 
-    Add a route to the routing table
-    @param string $route The route URL
-    @param array  $params Parameters (controller, action, etc.)
-    */
-
-    public function add ($route, $params = []) {
-
-        // convert the route to a regular expression: escape forward slashes 
+    /**
+     * Add a route to the routing table
+     *
+     * @param string $route  The route URL
+     * @param array  $params Parameters (controller, action, etc.)
+     *
+     * @return void
+     */
+    public function add($route, $params = [])
+    {
+        // Convert the route to a regular expression: escape forward slashes
         $route = preg_replace('/\//', '\\/', $route);
 
-
-        // convert variables e.g. {controller}
+        // Convert variables e.g. {controller}
         $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
 
-        // Add start and end delimiters, and case insensitive flag 
-        $route = '/' . $route . '$/i';
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^' . $route . '$/i';
 
         $this->routes[$route] = $params;
     }
 
-    public function getRoutes() {
+    /**
+     * Get all the routes from the routing table
+     *
+     * @return array
+     */
+    public function getRoutes()
+    {
         return $this->routes;
     }
 
-    /*
-    Match the route to the routes in the routing table, setting the $params
-    property if a route is found. 
-    
-    @param string $url the route URL
+    /**
+     * Match the route to the routes in the routing table, setting the $params
+     * property if a route is found.
+     *
+     * @param string $url The route URL
+     *
+     * @return boolean  true if a match found, false otherwise
+     */
+    public function match($url)
+    {
+        // Match to the fixed URL format /controller/action
+        //$reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
 
-    @return boolen true if a match found, false otherwise
-    */
-    public function match($url) {
-
-        // Match to the fixed url format `/controller/action`
-        // $reg_exp = "/public\/(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
-        
         foreach ($this->routes as $route => $params) {
             if (preg_match($route, $url, $matches)) {
 
-                foreach($matches as $key => $match) {
+                foreach ($matches as $key => $match) {
                     if (is_string($key)) {
                         $params[$key] = $match;
                     }
                 }
 
+                $this->params = $params;
+                return true;
             }
-
-            $this->params = $params;
-            return true;
         }
 
         return false;
@@ -71,10 +87,8 @@ class Router {
     /* Get the currently Matched parameters
        @return array
     */
-    public function getParams(){
+    public function getParams()
+    {
         return $this->params;
     }
-
 }
-
-?>
--- public/index.php
+++ public/index.php
@@ -1,38 +1,39 @@
 <?php
 
-require '../core/Router.php';
+/**
+ * Front controller
+ *
+ * PHP version 5.4
+ */
 
-$router =  new Router();
+/**
+ * Routing
+ */
+require '../Core/Router.php';
 
+$router = new Router();
 
-// Add the routes 
-// $router->add('', ['controller' => 'Home', 'action' => 'index']);
-// $router->add('public/posts', ['controller' => 'Posts', 'action' => 'index']);
-// $router->add('public/posts/new', ['controller' => 'Posts', 'action' => 'new']);
-$router->add('public/{controller}/{action}',);
-$router->add('public/admin/{action}/{controller}',);
-
-
-// Display the routing table 
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
+    
+// Display the routing table
 echo '<pre>';
-// var_dump($router->getRoutes());
+//var_dump($router->getRoutes());
 echo htmlspecialchars(print_r($router->getRoutes(), true));
 echo '</pre>';
 
-// get url from the addressbar
+
+// Match the requested route
 $url = $_SERVER['QUERY_STRING'];
 
-echo "the url is '$url'";
-
-if($router->match($url)){
+if ($router->match($url)) {
     echo '<pre>';
     var_dump($router->getParams());
     echo '</pre>';
-    
-}
-else {
+} else {
     echo "No route found for URL '$url'";
 }
-
-
-?>
