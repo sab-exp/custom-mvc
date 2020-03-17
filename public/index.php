dc1be26b299f6cc38ad13d64851e066b3bd47f1b
Sabbir Ahmed <sabbir_id@outlook.com>
2020-03-17 10:34:00 +0900

    Using autoload from vendor to autoload my classes

--- public/index.php
+++ public/index.php
@@ -3,18 +3,9 @@
 // PHP version 5.4
 
 
-// Twig
+// Requiring all the classes from Vendor and my classes
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-
-// Autoloder
-spl_autoload_register(function ($class) {
-    $root = dirname(__DIR__); // get the parent directory
-    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-    if (is_readable($file)) {
-        require $root . '/' . str_replace('\\', '/', $class) . '.php';
-    }
-});
 
 $router = new Core\Router();
 
